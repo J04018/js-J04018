4156cfe5f7d57da8a77b3a0516c862cc357feb97
J04018 <133060195+J04018@users.noreply.github.com>
2023-06-20 21:44:44 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 function setup() {
     createCanvas(600, 600);
-    background("black");
+    background("yellow");
   }
 
 
  function draw() {
-  stroke("blue");
-  fill("red");
+  stroke("grey");
+  fill("rgb(5,44,255)");
 
   if(mouseIsPressed){
     rect(mouseX, mouseY, 20, 35);
